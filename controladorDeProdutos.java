7b6ed986c66814989c931267ed08126930db4173
RegisDev777 <regisbale289@gmail.com>
2023-12-25 01:37:05 -0300

    Finalizado

--- controladorDeProdutos.java
+++ controladorDeProdutos.java
@@ -3,7 +3,9 @@ package br.com.mercadoTeste;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class controladorDeProdutos {
@@ -12,15 +14,37 @@ public class controladorDeProdutos {
 	   private HashMap<Integer, produtos> IDProduto = new HashMap<>();
 
 	   
+
+	   //GETTER 
 	   public List<produtos> getProdutosCadastrados() {
 		 return Collections.unmodifiableList(produtosCadastrados);
 	   }
 
 	   
+
 	   
 
 	   
-	   
+	   //METODOS DE ACRESENTA UNIDAE
+	   public void AcrescentaUnidade(int unidade) {
+		 for (produtos produtos : produtosCadastrados) {
+		    produtos.AddUnidade(unidade);		    
+	   	 }
+		 System.out.println("Estoque atualizado!");
+	   }
+	
+	   //METODO PRA VER TOTAL DE ESTOQUE
+	   public void produtsTotalEstoque() {
+		   int contador = 1;
+	      if(produtosCadastrados != null && !produtosCadastrados.isEmpty()) {
+	    	  for (produtos produtos : produtosCadastrados) { 
+				 System.out.println(contador + "º Produto:" + produtos.getProdutos() + "| Qtd: " + produtos.getQuantidade());
+				 contador++;
+			   }
+	      } else {
+	    	     System.out.println("Nenhum Produto cadastrado no momemto!");
+	      }  		   	
+	   }
 	   
 	   //METODO PARA ADICIONA PRODUTO HASHMAP
 	   public void adicionaProdutoHash(produtos produtosID) {
@@ -29,17 +53,74 @@ public class controladorDeProdutos {
 		    System.out.println(produtosID.getProdutos() + " add com sucesso!!");
 	   }	 
 	   
+	   //METODO DE REMOVE UNIDADEs
+	   public void RemoveUnidade(int unidade) {
+		   for (produtos produtos : produtosCadastrados) {
+			   if(produtos.getQuantidade() >= unidade) {
+				  produtos.removeUnidade(unidade);
+			      System.out.println("Baixa com sucesso!"); 
+			   } 
+			  produtos.removeUnidade(unidade);
+		   }
+	   }
+	   	   
+	   // METODO PARA MOSTRA O TOTAL DE INVESTIMENTO FEITO EM  DETERMINADO PRODUTO ( FINANCIAS )
+	   public produtos InvestimentoFeitoXLucroObtidoNoProduto(produtos produto) {
+            return produto;
+		}
+	   
+	   //METODO PARA RETORNA TOTAL DE VENDAS
+	   public void TotalDeVendas() {
+		    boolean semVenda = true;
+		    double totalVendas = 0.0;
+
+		    for (produtos produto : produtosCadastrados) {
+		        double vendas = produto.getModificadoresProdutos().getTotalVendaDeTodosOsProdutos();
+		        if (vendas > 0) {
+		            totalVendas += vendas;
+		            semVenda = false;
+		        }
+		    }
+
+		    if (semVenda) {
+		        System.out.println("| Sem vendas no momento!        |");
+		    } else {
+		        System.out.println(" | Total de vendas: " + totalVendas + "       |");
+		    }
+		}
+	 
+	   //METODO PARA RETORNA O VALOR LIQUIDO
+	   public void LucroDaVendas() {
+		    boolean semLucro = true;
+
+		    for (produtos produto : produtosCadastrados) {
+		        Double lucro = produto.LucrosLiquidosDasVendas();
+
+		        if (lucro != null && lucro > 0) {
+		            System.out.println("| Total de lucro Liquido: " + lucro + "  |");
+		            semLucro = false;
+		        }
+		    }
+
+		    if (semLucro) {
+		        System.out.println("| Sem lucro no momento!         |");
+		    }
+		}
+	   
+	   
+	   
+	   //METODOS DE BUSCA ( BUSCA PRODUTOS )
 	   
 	   //METODO PRA BUSCA O PRODUTO PELO ID
 	   public produtos buscaProdutosId (int idNumeroDoProduto) {
 	        if(IDProduto.get(idNumeroDoProduto) == null) {
-	        	System.out.println("RESULTADO: ID NAO ENCONTRADO");
+	        	  System.out.println("'" + idNumeroDoProduto + "' Id não encontrado.");
 	        } else {
 	        	 for (produtos produto : produtosCadastrados) {
 	        		     System.out.println();
 	 		        	 System.out.println("Dados do Produto Encontrado:");
 	 			          System.out.println("Nome: " + produto.getProdutos());
-	 			          System.out.println("Preço: " + produto.getPreco());
+	 			          System.out.println("Preço: " + produto.getPrecoViaFornecedor());
 	 			          System.out.println("Quantidade: " + produto.getQuantidade());
 	 			          System.out.println("Código do Produto: " + produto.getCodigoProduto());
 	 			          System.out.println("------------------------------");
@@ -54,13 +135,11 @@ public class controladorDeProdutos {
 		        	 System.out.println();
 		        	 System.out.println("Dados do Produto Encontrado:");
 			          System.out.println("Nome: " + produto.getProdutos());
-			          System.out.println("Preço: " + produto.getPreco());
+			          System.out.println("Preço: " + produto.getPrecoViaFornecedor());
 			          System.out.println("Quantidade: " + produto.getQuantidade());
 			          System.out.println("Código do Produto: " + produto.getCodigoProduto());
-			          System.out.println("------------------------------");
 		        }
 		    }	
-		    System.out.println("Produto não encontrado.");
 		    return null;
 		}    
 	   // MÉTODO PARA BUSCAR O PRODUTO PELO ID OU NOME
@@ -74,5 +153,74 @@ public class controladorDeProdutos {
 	       }
 	   } 
 	   
-	
+	   
+	   //METODO VERIFICA SE TEM NA LISTA PARA O ( ADICIONA PRODUTOS --> PRODUTOS EXISTENTES )
+	   public produtos VerificaId(int idNumeroDoProduto) {
+		    produtos produtoEncontrado = IDProduto.get(idNumeroDoProduto);
+		    if (produtoEncontrado == null) {
+		        System.out.println("'" + idNumeroDoProduto + "' Id não encontrado.");
+		    } else {
+		        System.out.println();
+		        System.out.println("Produto:");
+		        System.out.println("Nome: " + produtoEncontrado.getProdutos());
+		        System.out.println("Quantidade: " + produtoEncontrado.getQuantidade());
+		    }
+		    return produtoEncontrado;
+		}
+	   public produtos VerificaNome(String nomeProduto) {
+		    for (produtos produto : produtosCadastrados) {
+		        if (produto.getProdutos().equalsIgnoreCase(nomeProduto)) {
+		            System.out.println();
+		            System.out.println("Produto:");
+		            System.out.println("Nome: " + produto.getProdutos());
+		            System.out.println("Quantidade: " + produto.getQuantidade());
+		            System.out.println("------------------------------");
+		            return produto;
+		        }
+		    }
+		    System.out.println("'" + nomeProduto + "' Produto não encontrado.");
+		    return null;
+		}     
+	   public produtos VerificaIdOuNome(String chavePesquisa) {
+		    try {
+		        int idNumeroDoProduto = Integer.parseInt(chavePesquisa);
+		        return VerificaId(idNumeroDoProduto);
+		    } catch (NumberFormatException e) {
+		        // Se a conversão falhar, tratar como nome e realizar a busca
+		        return VerificaNome(chavePesquisa);
+		    }
+		
+	    }
+	    
+	    
+	   //METODO VERIFICA SE TEM NA LISTA PARA O ( FINANCIAS -> VENDAS DE PRODUTOS )
+	   public produtos financiaId(int idNumeroDoProduto) {
+		    produtos produtoEncontrado = IDProduto.get(idNumeroDoProduto);
+		    if (produtoEncontrado == null) {
+		        System.out.println("'" + idNumeroDoProduto + "' Id não encontrado.");
+		    }
+		    return produtoEncontrado;
+		}
+	   public produtos FinanciaNome(String nomeProduto) {
+		    for (produtos produto : produtosCadastrados) {
+		        if (produto.getProdutos().equalsIgnoreCase(nomeProduto)) {
+		            return produto;
+		        }
+		    }
+		    System.out.println("'" + nomeProduto + "' Produto não encontrado.");
+		    return null;
+		}     
+	   public produtos FinanicaIdOuNome(String chavePesquisa) {
+		    try {
+		        int idNumeroDoProduto = Integer.parseInt(chavePesquisa);
+		        return financiaId(idNumeroDoProduto);
+		    } catch (NumberFormatException e) {
+		        // Se a conversão falhar, tratar como nome e realizar a busca
+		        return FinanciaNome(chavePesquisa);
+		    }
+		
+	    }
+	     
+	    
+	    
 }
--- produtos.java
+++ produtos.java
@@ -5,42 +5,96 @@ public class produtos {
 	
 	
 	  private String produtos;
-      private double preco;
       private int    quantidade;      
       private int    codigoProduto;
+      private double precoViaForncedor;
+      private double precoAVenda;
+      private int QuantidadeInicial; 
+      private MetodosProdutos ModificadoresProdutos;
+
       
-      
-      public produtos(int codigoProduto,String produto, int quantidade, double preco ) {
+      //CONSTRUTOR
+      public produtos(int codigoProduto,String produto, int quantidade, double preco, double precoAVenda) {
 	     this.codigoProduto = codigoProduto;
     	 this.produtos = produto;
 	     this.quantidade = quantidade;   
-	     this.preco = preco;
- 
+	     this.QuantidadeInicial = quantidade;
+	     this.precoViaForncedor = preco;
+	     this.precoAVenda = precoAVenda;
+	     this.ModificadoresProdutos = new MetodosProdutos(); 
 	  }
       
        
-      
+      //GETTER E SETTERS
       public String getProdutos() {
 		return produtos;
 	}
-      public double getPreco() {
-		return preco;
+      public double getPrecoViaFornecedor() {
+		return precoViaForncedor;
 	}
+      public double getPrecoAVenda() {
+		return precoAVenda;
+	} 
       public int getQuantidade() {
 		return quantidade;
 	}
-      public int getCodigoProduto() {
-    	
+      public int getCodigoProduto() {	
 		return codigoProduto;
 	}
-
       
+      public MetodosProdutos getModificadoresProdutos() {
+		 return ModificadoresProdutos;
+	  }
+    
+    
+      //METODOS INTERNOS 
 
-//      @Override
-//      public String toString() {
-//          return "Produto: " + produtos + ", Preço: " + preco + ", Quantidade: " + quantidade + ", Código do Produto: " + codigoProduto;
-//      }
+  
+      //METODO PARA RETORNA O TOTAL DE LUCROS DAS VENDAS X REMOVE DO ESTOQUE UNIDADES
+      public double LucrosLiquidosDasVendas() {
+         return  getModificadoresProdutos().TotalVendaDeTodosOsProdutos - InvestiimentoInicialDOProduto();
+         
+      }
 
+      //METODO PARA RETORNA O TOTAL DE VALOR INVESTIDO NO ESTOQUE COM MUDANÇA SE ADICIONA MAIS
+      public double totalInvestimento() {
+    	  
+    	   return quantidade * precoViaForncedor;
+      }
+      
+      //VALOR DO INVESTIMENTO NAO MUDA
+      public double InvestiimentoInicialDOProduto() {
+         return QuantidadeInicial * precoViaForncedor;     
+      }
+      
+      
+      
+      //METODOS ADICIONA UNIDADE
+      public void AddUnidade(int quantidade) {
+    	  this.quantidade += quantidade;
+      } 
+      
+     //METODO PARA REMOVE ESTOQUE DE VENDA
+      public double removeUnidade(int quantidade) {
+    	    if (this.quantidade >= quantidade) {
+    	        this.quantidade -= quantidade;
+    	        double valorVendido = quantidade * precoAVenda;
 
+    	        // Atualiza os valores corretamente no ModificadoresProdutos
+    	        getModificadoresProdutos().adicionaValorVenda(valorVendido);
+    	        getModificadoresProdutos().adicionaQuantidadeBaixa(quantidade);
 
+    	        return valorVendido;
+    	    } else {
+    	        System.out.println("Quantidade insuficiente em estoque.");
+    	        return 0.0;
+    	    }
+    	}
+      
+     @Override
+      public String toString() {
+    	// TODO Auto-generated method stub
+    	return "Produto:" + getProdutos() + "\n" + "Ivestimento:" + InvestiimentoInicialDOProduto();
+     }
 }
+    
